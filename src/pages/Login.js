669bffe91fa7229fafe73e6c52b99d9922f48a4b
Seu Nome de Usuário <danielvortex@hotmail.com>
2023-09-26 16:57:50 -0300

    test

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { SiItunes } from 'react-icons/si';
-import { createUser } from '../services/userAPI';
-import Carregando from '../componets/Carregando';
-import './aquivos.css/login.css';
+import React from "react";
+import PropTypes from "prop-types";
+import { SiItunes } from "react-icons/si";
+import { createUser } from "../services/userAPI";
+import Carregando from "../componets/Carregando";
+import "./aquivos.css/login.css";
 
 class Login extends React.Component {
   constructor() {
@@ -11,7 +11,7 @@ class Login extends React.Component {
 
     this.state = {
       disabled: true,
-      user: '',
+      user: "",
       estaCarregando: false,
     };
     this.handleChange = this.handleChange.bind(this);
@@ -25,7 +25,6 @@ class Login extends React.Component {
     if (event.target.value.length >= numMinCarcter) {
       this.setState({
         disabled: false,
-
       });
     } else {
       this.setState({
@@ -40,61 +39,53 @@ class Login extends React.Component {
       estaCarregando: true,
     });
     const { user } = this.state;
-    console.log('clicando');
+    console.log("clicando");
 
     await createUser({ name: user });
     this.setState({
       estaCarregando: false,
     });
     const { history } = this.props;
-    history.push('/search');
-  }
+    history.push("/search");
+  };
 
   render() {
-    const {
-      user,
-      disabled,
-      estaCarregando,
-    } = this.state;
+    const { user, disabled, estaCarregando } = this.state;
 
     return (
-      <div data-testid="page-login" className="div-login">
-        <div className="div-img-login">
-          <h1>TrybeTUNES</h1>
-          <SiItunes />
+      <div className="container-page">
+        <div data-testid="page-login" className="div-login">
+          <div className="div-img-login">
+            <h1>TrybeTUNES</h1>
+            <SiItunes />
+          </div>
+          <section className="campo-login">
+            <form onSubmit={this.handleClick}>
+              <label htmlFor="login">
+                <input
+                  data-testid="login-name-input"
+                  type="text"
+                  name="login"
+                  id="login"
+                  value={user}
+                  onChange={this.handleChange}
+                  placeholder="Email"
+                />
+              </label>
+              <div className="div-button-login">
+                <button
+                  data-testid="login-submit-button"
+                  type="submit"
+                  disabled={disabled}
+                  onClick={this.handleClick}
+                >
+                  Entrar
+                </button>
+              </div>
+            </form>
+          </section>
+          {estaCarregando === true && <Carregando />}
         </div>
-        <section className="campo-login">
-          <form onSubmit={ this.handleClick }>
-            <label
-              htmlFor="login"
-            >
-
-              <input
-                data-testid="login-name-input"
-                type="text"
-                name="login"
-                id="login"
-                value={ user }
-                onChange={ this.handleChange }
-                placeholder="Email"
-              />
-            </label>
-            <div className="div-button-login">
-
-              <button
-                data-testid="login-submit-button"
-                type="submit"
-                disabled={ disabled }
-                onClick={ this.handleClick }
-              >
-                Entrar
-
-              </button>
-            </div>
-          </form>
-        </section>
-        {estaCarregando === true
-        && <Carregando />}
       </div>
     );
   }
